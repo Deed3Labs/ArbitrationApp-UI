3d3560b8fc888603b1412d4512519780dc663d8f
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-01-13 02:10:04 -0800

    Update Layout.tsx

--- packages/web-app/components/layout/agreements/Layout.tsx
+++ packages/web-app/components/layout/agreements/Layout.tsx
@@ -5,7 +5,7 @@ import { LayoutProps } from "../Layout";
 const AgreementsLayout = ({ children }: LayoutProps) => {
 	return (
 		<div className="w-full flex justify-center">
-			<div className="absolute top h-[300px] w-full bg-pr-c-green1 z-5">
+			<div className="absolute top h-[300px] w-full bg-pr-c-black1 z-5">
 				<Image src="/illustrations/header1.svg" fill object-fit="cover" alt={""} />
 			</div>
 			{children}
