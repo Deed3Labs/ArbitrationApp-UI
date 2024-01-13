e8d706dbf991c09f99c2de355850fb01e2dad191
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-01-13 04:47:34 -0800

    Update NavMenuButton.tsx

--- packages/ui-components/src/components/Molecules/buttons/NavMenuButton.tsx
+++ packages/ui-components/src/components/Molecules/buttons/NavMenuButton.tsx
@@ -5,7 +5,7 @@ import { Bars3Icon } from "@heroicons/react/24/solid";
 export const NavMenuButton = () => {
 	return (
 		<div className="w-fit">
-			<ButtonBase className="p-4 bg-white">
+			<ButtonBase className="p-4 bg-pr-c-black3">
 				<Bars3Icon className="w-8 h-8" />
 			</ButtonBase>
 		</div>
