989053c11f6097afbc015793a1cea4e51c8e0eac
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-01-13 10:01:28 -0800

    Update TopBar.tsx

--- packages/web-app/components/layout/TopBar.tsx
+++ packages/web-app/components/layout/TopBar.tsx
@@ -39,7 +39,7 @@ const DesktopNavigation = () => {
 						<IconRenderer
 							className=""
 							icon={<N3Agreement />}
-							backgroundColor="neutral-c-100"
+							backgroundColor="pr-c-green1"
 							size="xs"
 							rounded
 						/>
