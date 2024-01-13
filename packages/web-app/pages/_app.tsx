62abe75459f7ca6a24ef6543f8df6acd10f148c8
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-01-13 04:54:49 -0800

    Update _app.tsx

--- packages/web-app/pages/_app.tsx
+++ packages/web-app/pages/_app.tsx
@@ -42,7 +42,7 @@ const HeaderNavigation = memo(() => {
 		<>
 			<Link
 				href="/agreements"
-				className={`${"text-sm py-min2 px-min3 bg-white shadow rounded-md ml-min3 text-neutral-700"}`}
+				className={`${"text-sm py-min2 px-min3 bg-pr-c-black3 shadow rounded-md ml-min3 text-neutral-700"}`}
 			>
 				Agreements
 			</Link>
